36cfaad041f57981ed60bc1a81c5b2c239647ed6
Ben Crinion <77635121+BenCrinion-IW@users.noreply.github.com>
2024-12-19 09:50:32 +0000

    Added JUnit dependency

---
+++ test/GildedRoseTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GildedRoseTest {
+
+    @Test
+    void testSomething() {
+        assertEquals(1, 1);
+    }
+}
